ffce0b60db00ed0edb883530e61a1ca0bb73ecc6
SebaV657 <sebastian.velez17@upr.edu>
2025-11-19 14:48:56 -0400

    task 1 completed

--- Deck/DeckManager.py
+++ Deck/DeckManager.py
@@ -144,8 +144,19 @@ class DeckManager:
     #   object for each valid combination. If a matching image is not found, skip that card.
     #   Add each created Card to a list called 'deck' and return the completed list at the end.
     def createDeck(self, subLevel: SubLevel = None):
+
         cardImages = self.load_card_images(subLevel)
         deck = []
+
+        for suit in Suit:  # Suit.HEARTS, Suit.CLUBS, Suit.DIAMONDS, Suit.SPADES
+            for rank in Rank:  # Rank.TWO ... Rank.ACE
+                image = cardImages.get((suit, rank))
+
+                if image is None:
+                    continue
+
+                deck.append(Card(suit=suit, rank=rank, image=image))
+
         return deck
 
     # TODO (TASK 5.1): Complete the priceMap variable by assigning each joker a price.
